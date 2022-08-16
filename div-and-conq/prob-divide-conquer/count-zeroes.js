8e05f777e3b79217266c0ff9991027752f9655f0
Jae Kim <abc@example.com>
2022-08-16 16:43:02 -0700

    Added divide and conquer exercises

---
+++ div-and-conq/prob-divide-conquer/count-zeroes.js
@@ -0,0 +1,25 @@
+
+  
+function findFirstZero(arr, low, high) {
+    if (high >= low) {
+      
+      let mid = low + Math.floor((high - low) / 2)
+      if ((mid === 0 || arr[mid - 1] === 1) && arr[mid] === 0) {
+        return mid;
+      } else if (arr[mid] === 1) {  
+        return findFirstZero(arr, mid + 1, high)
+      }
+      return findFirstZero(arr, low, mid - 1)
+    }
+    return -1;
+  }
+
+  function countZeroes(arr) {
+    let n = arr.length;
+    let firstZero = findFirstZero(arr, 0, n-1)
+    if (firstZero === -1) {
+        return 0;  }
+    return n - firstZero
+  }
+
+module.exports = countZeroes
